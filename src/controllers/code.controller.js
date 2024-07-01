43dba3d33305fbcd1d27675419b0b0d87c5cc130
yash <yashchauhan1775@gmail.com>
2024-07-01 08:16:12 +0530

    workable web

--- src/controllers/code.controller.js
+++ src/controllers/code.controller.js
@@ -1,8 +1,39 @@
+// const Code = require('../models/code.model');
+// const Question = require('../models/question.model');
+
+// const addCode = async (req, res) => {
+//   const { questionId, description, language, code } = req.body;
+
+//   try {
+//     // Find the question by ID
+//     const question = await Question.findById(questionId);
+
+//     if (!question) {
+//       return res.status(404).json({ message: 'Question not found' });
+//     }
+
+//     // Create a new code entry
+//     const newCode = new Code({ description, language, code });
+//     await newCode.save();
+
+//     // Update the question's codes array
+//     question.codes.push(newCode._id);
+//     await question.save();
+
+//     res.status(201).json({ message: 'Code added to question successfully', code: newCode });
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: 'Failed to add code to question' });
+//   }
+// };
+
+// module.exports = { addCode };
 const Code = require('../models/code.model');
 const Question = require('../models/question.model');
 
 const addCode = async (req, res) => {
   const { questionId, description, language, code } = req.body;
+  console.log("Received code data:", { questionId, description, language, code });
 
   try {
     // Find the question by ID
@@ -20,10 +51,12 @@ const addCode = async (req, res) => {
     question.codes.push(newCode._id);
     await question.save();
 
+    console.log("Code added successfully:", newCode);
+
     res.status(201).json({ message: 'Code added to question successfully', code: newCode });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Failed to add code to question' });
+    console.error("Error in addCode:", error);
+    res.status(500).json({ error: 'Failed to add code to question', details: error.message });
   }
 };
 
